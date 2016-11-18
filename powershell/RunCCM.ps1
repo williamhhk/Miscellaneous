015b3c556422ff72f9b03aab303900334aedce82
williamhhk <william.han@gmail.com>
2016-11-18 05:15:37 -0700

    Update RunCCM.ps1

--- powershell/RunCCM.ps1
+++ powershell/RunCCM.ps1
@@ -29,7 +29,7 @@ if ($status.Contains("CcmSetup is exiting with return code 0"))
 {
     Try {Invoke-Command -Session $s -ErrorAction SilentlyContinue -ScriptBlock {Remove-Item -Recurse -Force c:\windows\ccm\* }}  Catch {}  
     Try {Invoke-Command -Session $s -ErrorAction SilentlyContinue -ScriptBlock {Remove-Item -Recurse -Force c:\windows\ccmcache\* }} Catch {}      
-    Try {Invoke-Command -Session $s -ErrorAction SilentlyContinue -ScriptBlock {Start-Process -FilePath "C:\windows\ccmsetup\ccmsetup.exe" -ArgumentList  "/mp:NVM5104CDC.us.global.schwab.com SMSSITECODE=WS2 FSP=NVM5501cdc.us.global.schwab.com SMSMP=NVM5104CDC.us.global.schwab.com SMSCACHESIZE=25600 CCMLOGMAXHISTORY=1 CCMLOGMAXSIZE=20000000 CCMDEBUGLOGGING=1 CCMLOGLEVEL=0 patch=\\nwtd0002pdv\rel_src\cmclient\configmgr2012ac-sp2r2sp1-kb3100144-x64.msp"}} Catch { Exit}
+    Try {Invoke-Command -Session $s -ErrorAction SilentlyContinue -ScriptBlock {Start-Process -FilePath "C:\windows\ccmsetup\ccmsetup.exe" -ArgumentList  ""}} Catch { Exit}
     $ccmsetupStatus = $null 
     do  
     {
