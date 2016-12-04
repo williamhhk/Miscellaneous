cfd3277782e688efb09847b4f4f1b6f406b3ce7f
williamhhk <william.han@gmail.com>
2016-12-04 15:49:20 -0700

    Update SCCMInboxesCount.ps1

--- powershell/SCCMInboxesCount.ps1
+++ powershell/SCCMInboxesCount.ps1
@@ -3,5 +3,6 @@ $server = "mypc"
 $path = "C:\Program Files\Microsoft Configuration Manager\inboxes"
 Invoke-Command -cn $server {
   param($path)
+ #  (gci c:\ -r | ? {$_.PSIsContainer -eq $True}) | ? {$_.GetFiles().Count -eq 0} | Group Directory | Sort-Object Count -Descending |  Format-Table Name, Count -autosize 
     Get-ChildItem  $path -recurse | Where {!$_.PSIsContainer} | Group Directory | Sort-Object Count -Descending |  Format-Table Name, Count -autosize
 } -ArgumentList $path
